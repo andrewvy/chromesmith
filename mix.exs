d6fdb41b68aba88504b9786942ac13d61f89e343
Andrew Vy <andrew@andrewvy.com>
2017-08-30 22:15:14 -0700

    :bookmark: v0.0.1
    Initial development release.

--- mix.exs
+++ mix.exs
@@ -4,10 +4,14 @@ defmodule Chromesmith.Mixfile do
   def project do
     [
       app: :chromesmith,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "Chromesmith",
+      source_url: "https://github.com/andrewvy/chromesmith",
+      description: description(),
+      package: package()
     ]
   end
 
@@ -22,7 +26,22 @@ defmodule Chromesmith.Mixfile do
   defp deps do
     [
       {:chrome_launcher, "~> 0.0.1"},
-      {:chrome_remote_interface, "~> 0.0.2"}
+      {:chrome_remote_interface, "~> 0.0.2"},
+      {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
+  end
+
+  defp description do
+    "Higher-level library for forging distributing workloads across an army of managed headless chrome workers."
+  end
+
+  defp package do
+    [
+      maintainers: ["andrew@andrewvy.com"],
+      licenses: ["MIT"],
+      links: %{
+        "Github" => "https://github.com/andrewvy/chromesmith"
+      }
     ]
   end
 end
